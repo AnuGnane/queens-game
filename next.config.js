55991cdf33988edd2303fa9cb643535a90aa4e95
Anu Gnana <anugnana@Anus-MBP.cust.communityfibre.co.uk>
2024-09-06 20:49:35 +0100

    Updated configuration for static export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
- 
+  basePath: process.env.NODE_ENV === 'production' ? '/queens-game' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/queens-game/' : '',
 }
- 
+
 module.exports = nextConfig
