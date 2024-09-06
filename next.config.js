7d3b89719f03a49c33e28a6092437e9f030e572e
Anu Gnana <anugnana@Anus-MBP.cust.communityfibre.co.uk>
2024-09-06 18:57:54 +0100

    prep

--- next.config.js
+++ next.config.js
@@ -5,5 +5,11 @@ const nextConfig = {
     unoptimized: true,
   },
 }
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/{queens-game}' : '',
+  basePath: isProd ? '/{queens-game}' : '',
+}
 
 module.exports = nextConfig
